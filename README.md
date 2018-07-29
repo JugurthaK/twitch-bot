d15c905c317ce6a7bf72704d9523f9f96a36226f
JugurthaK <jugurtha.halzoun@gmail.com>
2018-07-29 13:24:01 +0200

    First

---
+++ README.md
@@ -0,0 +1,14 @@
+# Fortnite Twitch Bot
+*A twitch bot to use Fortnite API*
+*Made with tmi.js & fortnite-api (available on npm)*
+
+## Get your Client & Fortnite Token, 
+- For it, you must check fortnite-api doc on npm or git (https://www.npmjs.com/package/fortnite-api)
+
+## Get your twitch oauth Token,
+- Create an account for your bot on twitch, and then click on http://twitchapps.com/tmi/
+
+## Configure your bot
+- Check on bot.js and complete missing tokens & epic games account
+- Type "node bot.js" on terminal or cmd (on root of project obviously)
+- ENJOY
---
+++ bot.js
@@ -0,0 +1,71 @@
+// token : oauth:zog4g7dflqgklppyl09rrr4b3ovgx3
+// client fortnite : basic MzRhMDJjZjhmNDQxNGUyOWIxNTkyMTg3NmRhMzZmOWE6ZGFhZmJjY2M3Mzc3NDUwMzlkZmZlNTNkOTRmYzc2Y2Y=
+// game fortnite : basic ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ=
+
+const tmi = require("tmi.js");
+const fortnite = require("fortnite-api");
+const tmiConfig = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect:  true
+    },
+    identity: {
+        username: "juk_bot",
+        password: "TWITCHTOKEN"
+    },
+    channels: [
+        "JU_GURTHA"
+    ]
+};
+
+let fortniteAPI = new fortnite([
+    "EMAIL",
+    "PASS",
+    "CLITOKEN",
+    "FTTOKEN"
+],{
+    debug : true
+});
+
+
+
+function isSubscriber(user){
+    return user.subscriber;
+}
+
+function isStreamer(user){
+    return user.badges.broadcaster == '1';
+}
+
+let client = new tmi.client(tmiConfig);
+
+client.connect();
+
+client.on("chat", (channel, user, message) => {
+    if (message.includes("!fstats")){
+        let args = message.split(" ");
+        let pseudo = args[1];
+        let plateforme = args[2];
+        
+        if (isSubscriber(user) || isStreamer(user)){
+            fortniteAPI.login().then(() => {
+                if (fortniteAPI.checkPlayer(pseudo, plateforme)){
+                    fortniteAPI.getStatsBR(pseudo, plateforme, "alltime")
+                    .then (stats => {
+                        client.say(channel, user['display-name'] + " - Ratio: "+ stats.lifetimeStats['k/d'] + " // Wins: " + stats.lifetimeStats.wins + " // Kills: " + stats.lifetimeStats.kills);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        client.say(channel, user['display-name'] + " - Ce joueur n'existe pas");
+                    })
+                }
+            }) 
+        } else {
+            client.say(channel, "Désolé, tu n'as pas accès à cette commande");
+        }
+    } 
+});
+
+
